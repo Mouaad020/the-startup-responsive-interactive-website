a4a12e6dc119548c30d9f6bcc15612a6141f18ba
Mouaad020 <mouaad.chakroun@hva.nl>
2026-01-18 21:33:53 +0100

    #6 feedback verwerkt

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const send = document.querySelector('.locatie')
 const hidecontent = document.querySelector('.hidden')
 const showcontent = document.querySelector('.show')
-
+const track = document.querySelector('.li-track')
 const volgenform = document.querySelector('.form-volgen')
 const pakketform = document.querySelector('.form-pakketpunt')
 
@@ -10,10 +10,26 @@ const pakketform = document.querySelector('.form-pakketpunt')
 
 
 
-
 send.addEventListener('click', function() {
     pakketform.classList.add('shown')
+    pakketform.classList.remove('hidden')
     volgenform.classList.add('hidden')
+    volgenform.classList.remove('shown')
+    track.classList.add('hidden')
+    track.classList.remove('show')
+    send.classList.add('show')
+    send.classList.remove('hidden')
 });
 
-//daarna komt de nieuwe form tevoorschijn//
+
+
+track.addEventListener('click', function() {
+    volgenform.classList.add('shown')
+    volgenform.classList.remove('hidden')
+    pakketform.classList.add('hidden')
+    pakketform.classList.remove('shown')
+    track.classList.add('show')
+    track.classList.remove('hidden')
+    send.classList.add('hidden')
+    send.classList.remove('show')
+});
